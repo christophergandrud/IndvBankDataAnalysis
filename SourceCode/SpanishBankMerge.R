a7f9089acca9cfa3337968bedeb0f5e365211685
Christopher Gandrud <christopher.gandrud@gmail.com>
2013-11-18 16:44:23 +0100

    spain

--- SourceCode/SpanishBankMerge.R
+++ SourceCode/SpanishBankMerge.R
@@ -1,21 +1,28 @@
 ###########
 # Combine Spanish Commercial Bank Data
 # Christopher Gandrud
-# 12 November 2013
+# 18 November 2013
 ##########
 
 # Load packages
+library(gdata)
 library(reshape2)
 library(stringr)
 library(DataCombine)
+library(plyr)
+library(lubridate)
 
 setwd("~/Dropbox/AMCProject/FailuresAndFederalism/IndvBankDataAnalysis/Data/Spain/CommercialBanks/Domestic/")
 
-
 Files <- list.files()
 
+Combined <- data.frame()
+
 for (i in Files){
-  Temp <- read.csv(i)
+  Temp <- read.csv(i, stringsAsFactors = FALSE, fileEncoding = "latin1")
+  ColNames <- Temp[1, 2:length(Temp)]
+  ColNames <- c("Var", ColNames)
+  names(Temp) <- ColNames
   Temp <- Temp[-1, ]
   Temp$Var <- gsub("[0-9]", "", Temp$Var)
   Temp$Var <- gsub("\\.", "", Temp$Var)
@@ -30,6 +37,23 @@ for (i in Files){
   # Create month variable
   i <- gsub("\\.csv", "", i)
   i <- gsub("2", "-2", i)
+  i <- gsub("201-2", "2012", i)
+  i <- paste0("1-", i)
   TempCast$date <- i
   TempCast <- MoveFront(TempCast, "date")
+  
+  # Merge Together
+  Combined <- rbind.fill(Combined, TempCast)
+  
+  # Remove temps
+  rmExcept("Combined", message = FALSE)
 }
+
+# Clean Data
+Combined$date <- dmy(Combined$date)
+Combined <- Combined[order(Combined$bank, Combined$date), ]
+
+
+#### Tests ####
+
+
